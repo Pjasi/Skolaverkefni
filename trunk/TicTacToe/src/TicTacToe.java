3585f40a1d4335cdb1e93072416c925e25a38eed
Bjorn Thorsteinsson <bjozzi@gmail.com>
2011-11-11 16:51:53 +0000

    fyrsta commit a java umhverfi

---
+++ trunk/TicTacToe/src/TicTacToe.java
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Notandi
+ * Date: 11.11.2011
+ * Time: 16:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class TicTacToe {
+    int i = 0;
+
+}
