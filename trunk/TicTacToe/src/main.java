070d48cae910ea8270dd555ee0018964a1bd5081
Asbjorn <asbjornhp@gmail.com>
2011-11-11 17:27:33 +0000

    Beinagrind af TicTacToe

---
+++ trunk/TicTacToe/src/main.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+
+
+/**
+ * @author asbjornpetursson
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TicTacToe");
+		printBoard();
+
+	    while (!isWinner()) {
+	        readMove(); // hreyfing spilara
+	        printBoard(); // prenta bord
+	    }
+
+	    printWinner(); // prenta hver vann
+	}
+
+	private static void printWinner() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void printBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void readMove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static boolean isWinner() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+	}
+
+
